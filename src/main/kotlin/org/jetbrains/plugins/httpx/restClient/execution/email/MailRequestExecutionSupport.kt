22eaabd3f6a25f4d0a874ef9407344ec7094ed6a
linux_china <libing.chen@gmail.com>
2022-03-03 16:11:14 -0800

    MailRequestExecutionSupport

---
+++ src/main/kotlin/org/jetbrains/plugins/httpx/restClient/execution/email/MailRequestExecutionSupport.kt
@@ -0,0 +1,26 @@
+package org.jetbrains.plugins.httpx.restClient.execution.email
+
+import com.intellij.httpClient.execution.common.RequestContext
+import com.intellij.httpClient.execution.common.RequestConverter
+import com.intellij.httpClient.execution.common.RequestExecutionSupport
+import com.intellij.httpClient.execution.common.RequestHandler
+
+@Suppress("UnstableApiUsage")
+class MailRequestExecutionSupport : RequestExecutionSupport<MailRequest> {
+
+    override fun canProcess(requestContext: RequestContext): Boolean {
+        return requestContext.method == "MAIL"
+    }
+
+    override fun getRequestConverter(): RequestConverter<MailRequest> {
+        return MailRequestConverter()
+    }
+
+    override fun getRequestHandler(): RequestHandler<MailRequest> {
+        return MailRequestHandler()
+    }
+
+    override fun supportedMethods(): Collection<String> {
+        return listOf("MAIL")
+    }
+}
