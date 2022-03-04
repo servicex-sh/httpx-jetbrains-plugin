11ee6afadf0aa6c3cabb364eb4784f73354707ad
linux_china <libing.chen@gmail.com>
2022-03-03 17:23:40 -0800

    Polishing

--- src/main/kotlin/org/jetbrains/plugins/httpx/restClient/execution/publish/PublishRequestManager.kt
+++ src/main/kotlin/org/jetbrains/plugins/httpx/restClient/execution/publish/PublishRequestManager.kt
@@ -7,9 +7,8 @@ import com.intellij.openapi.project.Project
 import org.eclipse.paho.mqttv5.client.MqttClient
 import org.eclipse.paho.mqttv5.client.MqttConnectionOptions
 import org.eclipse.paho.mqttv5.client.persist.MemoryPersistence
-import org.eclipse.paho.mqttv5.common.MqttException
 import org.eclipse.paho.mqttv5.common.MqttMessage
-import java.net.URI
+import org.jetbrains.plugins.httpx.restClient.execution.common.getMqttUri
 
 @Suppress("UnstableApiUsage")
 class PublishRequestManager(private val project: Project) : Disposable {
@@ -32,40 +31,21 @@ class PublishRequestManager(private val project: Project) : Disposable {
     }
 
     private fun sendMqttMessage(request: PublishRequest): CommonClientResponse {
-        var sampleClient: MqttClient? = null
+        var mqttClient: MqttClient? = null
         try {
             val uri = getMqttUri(request.uri!!)
-            sampleClient = MqttClient(uri, "httpx-plugin", MemoryPersistence())
-            val connOpts = MqttConnectionOptions()
-            connOpts.isCleanStart = true
-            sampleClient.connect(connOpts)
-            sampleClient.publish(request.topic, MqttMessage(request.bodyBytes()))
+            mqttClient = MqttClient(uri, "httpx-plugin", MemoryPersistence())
+            val connOpts = MqttConnectionOptions().apply {
+                isCleanStart = true
+            }
+            mqttClient.connect(connOpts)
+            mqttClient.publish(request.topic, MqttMessage(request.bodyBytes()))
             return PublishResponse()
         } catch (e: Exception) {
             return PublishResponse(CommonClientResponseBody.Empty(), "Error", e.stackTraceToString())
         } finally {
-            if (sampleClient != null) {
-                try {
-                    sampleClient.disconnect()
-                } catch (ignore: MqttException) {
-                }
-            }
+            mqttClient?.disconnect()
         }
     }
-
-
-    private fun getMqttUri(mqttURI: URI): String {
-        val schema = mqttURI.scheme
-        var brokerUrl = mqttURI.toString()
-        brokerUrl = if (schema.contains("+")) {
-            brokerUrl.substring(brokerUrl.indexOf("+") + 1)
-        } else {
-            brokerUrl.replace("mqtt://", "tcp://")
-        }
-        return brokerUrl
-    }
-
 
 }
-
-data class UriAndSubject(val uri: String, val subject: String)
