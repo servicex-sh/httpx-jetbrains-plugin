6c64ea6e3abb7843d743f773505ccab522928671
linux_china <libing.chen@gmail.com>
2022-03-03 17:54:15 -0800

    CloudEventsJsonSchemaProviderFactory

---
+++ src/main/kotlin/org/jetbrains/plugins/httpx/json/CloudEventsJsonSchemaProviderFactory.kt
@@ -0,0 +1,30 @@
+package org.jetbrains.plugins.httpx.json
+
+import com.intellij.httpClient.http.request.psi.HttpMessageBody
+import com.intellij.httpClient.http.request.psi.HttpRequest
+import com.intellij.json.psi.JsonFile
+import com.intellij.lang.injection.InjectedLanguageManager
+import com.intellij.openapi.vfs.VfsUtil
+import com.intellij.openapi.vfs.VirtualFile
+import com.intellij.psi.PsiFile
+import com.intellij.psi.util.PsiTreeUtil
+import com.jetbrains.jsonSchema.extension.ContentAwareJsonSchemaFileProvider
+
+@Suppress("UnstableApiUsage")
+class CloudEventsJsonSchemaProviderFactory : ContentAwareJsonSchemaFileProvider {
+    override fun getSchemaFile(psiFile: PsiFile): VirtualFile? {
+        if (psiFile is JsonFile) {
+            val injectedLanguageManager = InjectedLanguageManager.getInstance(psiFile.project)
+            val psiLanguageInjectionHost = injectedLanguageManager.getInjectionHost(psiFile)
+            if (psiLanguageInjectionHost is HttpMessageBody) {
+                val httpMessageBody: HttpMessageBody = psiLanguageInjectionHost
+                val httpRequest = PsiTreeUtil.getParentOfType(httpMessageBody, HttpRequest::class.java)!!
+                val headerField = httpRequest.getHeaderField("Content-Type")
+                if (headerField != null && headerField.headerFieldValue?.text == "application/cloudevents+json") {
+                    return VfsUtil.findFileByURL(this.javaClass.getResource("/cloudevents-schema.json")!!)
+                }
+            }
+        }
+        return null
+    }
+}
