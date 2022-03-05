2dc3a7949012b33f1a67d2be23b37864395a2711
linux_china <libing.chen@gmail.com>
2022-03-04 17:22:39 -0800

    Add rabbitmq, kafka, redis subscribe

--- src/main/kotlin/org/jetbrains/plugins/httpx/restClient/execution/subscribe/SubscribeRequestManager.kt
+++ src/main/kotlin/org/jetbrains/plugins/httpx/restClient/execution/subscribe/SubscribeRequestManager.kt
@@ -4,9 +4,13 @@ import com.intellij.httpClient.execution.common.CommonClientResponse
 import com.intellij.httpClient.execution.common.CommonClientResponseBody
 import com.intellij.openapi.Disposable
 import com.intellij.openapi.project.Project
+import com.intellij.util.queryParameters
+import com.rabbitmq.client.ConnectionFactory
 import io.nats.client.Nats
 import kotlinx.coroutines.channels.BufferOverflow
 import kotlinx.coroutines.flow.MutableSharedFlow
+import org.apache.kafka.clients.consumer.ConsumerConfig
+import org.apache.kafka.common.serialization.StringDeserializer
 import org.eclipse.paho.mqttv5.client.MqttClient
 import org.eclipse.paho.mqttv5.client.MqttConnectionOptions
 import org.eclipse.paho.mqttv5.client.persist.MemoryPersistence
@@ -14,6 +18,13 @@ import org.eclipse.paho.mqttv5.common.MqttMessage
 import org.jetbrains.plugins.httpx.restClient.execution.common.TextBodyFileHint
 import org.jetbrains.plugins.httpx.restClient.execution.common.getMqttUri
 import org.jetbrains.plugins.httpx.restClient.execution.publish.AbstractMqttCallback
+import reactor.core.scheduler.Schedulers
+import reactor.kafka.receiver.KafkaReceiver
+import reactor.rabbitmq.RabbitFlux
+import reactor.rabbitmq.Receiver
+import reactor.rabbitmq.ReceiverOptions
+import redis.clients.jedis.Jedis
+import redis.clients.jedis.JedisPubSub
 import java.nio.charset.StandardCharsets
 import java.util.*
 
@@ -35,6 +46,12 @@ class SubscribeRequestManager(private val project: Project) : Disposable {
             return subscribeMqtt(request)
         } else if (schema.startsWith("nats")) {
             return subscribeNats(request)
+        } else if (schema.startsWith("amqp")) {
+            return subscribeRabbitmq(request)
+        } else if (schema.startsWith("kafka")) {
+            return subscribeKafka(request)
+        } else if (schema.startsWith("redis")) {
+            return subscribeRedis(request)
         }
         return SubscribeResponse(CommonClientResponseBody.Text("Schema not support!"), "401", "Unknown schema")
     }
@@ -71,27 +88,125 @@ class SubscribeRequestManager(private val project: Project) : Disposable {
     }
 
     private fun subscribeNats(request: SubscribeRequest): CommonClientResponse {
-        val topic: String = request.topic!!
+        val shared = MutableSharedFlow<CommonClientResponseBody.TextStream.Message>(
+            replay = 1000,
+            onBufferOverflow = BufferOverflow.DROP_OLDEST
+        )
+        var nc: io.nats.client.Connection? = null
+        val disposeConnection = Disposable {
+            nc?.close()
+        }
+        val textStream = CommonClientResponseBody.TextStream(shared, TextBodyFileHint.textBodyFileHint("nats-result.txt")).withConnectionDisposable(disposeConnection)
         try {
-            val shared = MutableSharedFlow<CommonClientResponseBody.TextStream.Message>(
-                replay = 1000,
-                onBufferOverflow = BufferOverflow.DROP_OLDEST
-            )
-            var nc: io.nats.client.Connection? = null
-            val disposeConnection = Disposable {
-                nc?.close()
-            }
-            val textStream = CommonClientResponseBody.TextStream(shared, TextBodyFileHint.textBodyFileHint("nats-result.txt")).withConnectionDisposable(disposeConnection)
             nc = Nats.connect(request.uri.toString())
             val dispatcher = nc.createDispatcher {
                 val body = it.data.toString(StandardCharsets.UTF_8)
                 shared.tryEmit(CommonClientResponseBody.TextStream.Message.Chunk(body + "\n\n"))
             }
-            dispatcher.subscribe(topic)
-            return SubscribeResponse(textStream)
+            dispatcher.subscribe(request.topic!!)
         } catch (e: java.lang.Exception) {
-            return SubscribeResponse(CommonClientResponseBody.Empty(), "Error", e.stackTraceToString())
+            shared.tryEmit(CommonClientResponseBody.TextStream.Message.ConnectionClosed.WithError(e))
         }
+        return SubscribeResponse(textStream)
+    }
+
+    private fun subscribeRedis(request: SubscribeRequest): CommonClientResponse {
+        val shared = MutableSharedFlow<CommonClientResponseBody.TextStream.Message>(
+            replay = 1000,
+            onBufferOverflow = BufferOverflow.DROP_OLDEST
+        )
+        var jedis: Jedis? = null
+        val disposeConnection = Disposable {
+            jedis?.close()
+        }
+        val textStream = CommonClientResponseBody.TextStream(shared, TextBodyFileHint.textBodyFileHint("redis-result.txt")).withConnectionDisposable(disposeConnection)
+        try {
+            jedis = Jedis(request.uri.toString())
+            jedis.subscribe(object : JedisPubSub() {
+                override fun onMessage(channel: String, message: String) {
+                    shared.tryEmit(CommonClientResponseBody.TextStream.Message.Chunk(message + "\n\n"))
+                }
+            }, request.topic)
+        } catch (e: Exception) {
+            shared.tryEmit(CommonClientResponseBody.TextStream.Message.ConnectionClosed.WithError(e))
+        }
+        return SubscribeResponse(textStream)
+    }
+
+    private fun subscribeRabbitmq(request: SubscribeRequest): CommonClientResponse {
+        val shared = MutableSharedFlow<CommonClientResponseBody.TextStream.Message>(
+            replay = 1000,
+            onBufferOverflow = BufferOverflow.DROP_OLDEST
+        )
+        var receiver: Receiver? = null
+        val disposeConnection = Disposable {
+            receiver?.close()
+        }
+        val textStream = CommonClientResponseBody.TextStream(shared, TextBodyFileHint.textBodyFileHint("redis-result.txt")).withConnectionDisposable(disposeConnection)
+        try {
+            val connectionFactory = ConnectionFactory()
+            connectionFactory.setUri(request.uri)
+            val receiverOptions = ReceiverOptions()
+                .connectionFactory(connectionFactory)
+                .connectionSubscriptionScheduler(Schedulers.boundedElastic())
+            receiver = RabbitFlux.createReceiver(receiverOptions)
+            receiver.consumeAutoAck(request.topic!!)
+                .doOnError {
+                    shared.tryEmit(CommonClientResponseBody.TextStream.Message.ConnectionClosed.WithError(it))
+                    receiver?.close()
+                }
+                .subscribe {
+                    val data = String(it.body, StandardCharsets.UTF_8)
+                    shared.tryEmit(CommonClientResponseBody.TextStream.Message.Chunk(data + "\n\n"))
+                }
+        } catch (e: Exception) {
+            shared.tryEmit(CommonClientResponseBody.TextStream.Message.ConnectionClosed.WithError(e))
+        }
+        return SubscribeResponse(textStream)
+    }
+
+    private fun subscribeKafka(request: SubscribeRequest): CommonClientResponse {
+        val shared = MutableSharedFlow<CommonClientResponseBody.TextStream.Message>(
+            replay = 1000,
+            onBufferOverflow = BufferOverflow.DROP_OLDEST
+        )
+        var fluxDisposable: reactor.core.Disposable? = null
+        val disposeConnection = Disposable {
+            fluxDisposable?.dispose();
+        }
+        val textStream = CommonClientResponseBody.TextStream(shared, TextBodyFileHint.textBodyFileHint("kafka-result.txt")).withConnectionDisposable(disposeConnection)
+        val props = Properties()
+        val kafkaURI = request.uri!!
+        var port: Int = kafkaURI.port
+        if (port <= 0) {
+            port = 9092
+        }
+        val topic: String = kafkaURI.getPath().substring(1)
+        val params: Map<String, String> = kafkaURI.queryParameters
+        var groupId: String? = "httpx-" + UUID.randomUUID()
+        if (params.containsKey("group")) {
+            groupId = params["group"]
+        }
+        props[ConsumerConfig.BOOTSTRAP_SERVERS_CONFIG] = kafkaURI.getHost() + ":" + port
+        props[ConsumerConfig.GROUP_ID_CONFIG] = groupId
+        props[ConsumerConfig.ENABLE_AUTO_COMMIT_CONFIG] = "true"
+        props[ConsumerConfig.KEY_DESERIALIZER_CLASS_CONFIG] = StringDeserializer::class.java.name
+        props[ConsumerConfig.VALUE_DESERIALIZER_CLASS_CONFIG] = StringDeserializer::class.java.name
+        val receiverOptions = reactor.kafka.receiver.ReceiverOptions.create<String, String>(props).subscription(setOf(topic))
+        try {
+            val kafkaReceiver: KafkaReceiver<String, String> = KafkaReceiver.create(receiverOptions)
+            fluxDisposable = kafkaReceiver.receive()
+                .doOnError {
+                    shared.tryEmit(CommonClientResponseBody.TextStream.Message.ConnectionClosed.WithError(it))
+                    fluxDisposable?.dispose()
+                }
+                .subscribe {
+                    shared.tryEmit(CommonClientResponseBody.TextStream.Message.Chunk(it.value() + "\n\n"))
+                }
+        } catch (e: Exception) {
+            shared.tryEmit(CommonClientResponseBody.TextStream.Message.ConnectionClosed.WithError(e))
+        }
+        return SubscribeResponse(textStream)
     }
 
 }
