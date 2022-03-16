fc025b210a79f8d2c98d9381347a152537504f8a
linux_china <libing.chen@gmail.com>
2022-03-16 13:50:46 -0700

    Implement aliyun request

--- src/main/kotlin/org/jetbrains/plugins/httpx/restClient/execution/aliyun/AliyunRequestManager.kt
+++ src/main/kotlin/org/jetbrains/plugins/httpx/restClient/execution/aliyun/AliyunRequestManager.kt
@@ -1,18 +1,17 @@
 package org.jetbrains.plugins.httpx.restClient.execution.aliyun
 
+import com.aliyuncs.CommonRequest
+import com.aliyuncs.DefaultAcsClient
+import com.aliyuncs.IAcsClient
+import com.aliyuncs.profile.DefaultProfile
+import com.fasterxml.jackson.databind.ObjectMapper
 import com.intellij.httpClient.execution.common.CommonClientResponse
 import com.intellij.httpClient.execution.common.CommonClientResponseBody
 import com.intellij.openapi.Disposable
 import com.intellij.openapi.project.Project
-import io.netty.buffer.Unpooled
-import io.netty.handler.ssl.SslContextBuilder
-import io.netty.handler.ssl.util.InsecureTrustManagerFactory
+import com.intellij.util.queryParameters
 import org.jetbrains.plugins.httpx.restClient.execution.common.JsonBodyFileHint
-import reactor.core.publisher.Mono
-import reactor.netty.http.client.HttpClient
-import reactor.netty.tcp.SslProvider.SslContextSpec
-import java.net.URI
-import java.nio.charset.StandardCharsets
+import org.jetbrains.plugins.httpx.restClient.execution.common.XmlBodyFileHint
 
 @Suppress("UnstableApiUsage")
 class AliyunRequestManager(private val project: Project) : Disposable {
@@ -20,33 +19,55 @@ class AliyunRequestManager(private val project: Project) : Disposable {
     override fun dispose() {
     }
 
-    fun execute(graphqlRequest: AliyunRequest): CommonClientResponse {
-        val client = httpClient()
-        val uri = graphqlRequest.uri
-        return httpPost(client, uri, graphqlRequest);
-    }
-
-
-    private fun httpPost(httpClient: HttpClient, requestUri: URI, httpRequest: AliyunRequest): CommonClientResponse {
-        val responseReceiver = httpClient.post().uri(requestUri).send(Mono.just(Unpooled.wrappedBuffer(httpRequest.bodyBytes())))
-        return responseReceiver.responseSingle { response, content ->
-            content.asByteArray().map {
-                val body = it.toString(StandardCharsets.UTF_8)
-                AliyunResponse(
-                    response.responseHeaders(),
-                    CommonClientResponseBody.Text(body, JsonBodyFileHint.jsonBodyFileHint("graphql-result.json")),
-                    response.status().toString()
-                )
-            }
-        }.block()!!
-    }
-
-
-    private fun httpClient(): HttpClient {
-        return HttpClient.create().secure { sslContextSpec: SslContextSpec ->
-            sslContextSpec.sslContext(SslContextBuilder.forClient().trustManager(InsecureTrustManagerFactory.INSTANCE).build())
+    fun execute(aliyunRequest: AliyunRequest): CommonClientResponse {
+        val keyIdAndSecret: List<String>? = Aliyun.readAliyunAccessToken(aliyunRequest)
+        if (keyIdAndSecret == null) {
+            return AliyunResponse(null, CommonClientResponseBody.Empty(), "Error", "No Aliyun AK found!")
         }
+        val host: String = aliyunRequest.uri.host
+        val serviceName = Aliyun.getServiceName(host)
+        val profile = DefaultProfile.getProfile(
+            aliyunRequest.regionId,
+            keyIdAndSecret[0],
+            keyIdAndSecret[1]
+        )
+        val queries = aliyunRequest.uri.queryParameters
+        val client: IAcsClient = DefaultAcsClient(profile)
+        val request = CommonRequest()
+        request.sysDomain = host
+        request.sysAction = queries["Action"]
+        val version = if (queries.containsKey("Version")) {
+            queries["Version"]
+        } else {
+            Aliyun.getApiVersion(serviceName)
+        }
+        request.sysVersion = version
+        var format: String? = "JSON"
+        if (queries.containsKey("Format")) {
+            format = queries["Format"]!!
+        } else {
+            val acceptHeader: String? = aliyunRequest.headers["Accept"]
+            if (acceptHeader != null && acceptHeader.contains("xml")) {
+                format = "XML"
+            }
+        }
+        request.putQueryParameter("Format", format)
+        val bodyBytes: ByteArray = aliyunRequest.bodyBytes()
+        if (bodyBytes.isNotEmpty()) {
+            val requestData = ObjectMapper().readValue(bodyBytes, Map::class.java)
+            for ((key, value) in requestData) {
+                request.putQueryParameter(key.toString(), value.toString())
+            }
+        }
+        val response = client.getCommonResponse(request)
+        val sysHeaders = response.httpResponse.sysHeaders
+        val text = response.data
+        val bodyFileHint = if (format == "JSON") {
+            JsonBodyFileHint.jsonBodyFileHint("aliyun-result.json")
+        } else {
+            XmlBodyFileHint.xmlBodyFileHint("aliyun-result.xml")
+        }
+        return AliyunResponse(sysHeaders, CommonClientResponseBody.Text(text, bodyFileHint))
     }
-
 
 }
