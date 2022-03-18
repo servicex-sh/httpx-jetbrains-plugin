211fffb107ba1d0618724fd974c35afbb3ac5f4d
linux_china <libing.chen@gmail.com>
2022-03-17 22:34:27 -0700

    Add getMessageBodyFiletHint

--- src/main/kotlin/org/jetbrains/plugins/httpx/restClient/execution/subscribe/SubscribeRequest.kt
+++ src/main/kotlin/org/jetbrains/plugins/httpx/restClient/execution/subscribe/SubscribeRequest.kt
@@ -1,6 +1,9 @@
 package org.jetbrains.plugins.httpx.restClient.execution.subscribe
 
+import com.intellij.httpClient.execution.common.CommonClientBodyFileHint
 import com.intellij.httpClient.execution.common.CommonClientRequest
+import org.jetbrains.plugins.httpx.restClient.execution.common.JsonBodyFileHint
+import org.jetbrains.plugins.httpx.restClient.execution.common.TextBodyFileHint
 import java.net.URI
 
 @Suppress("UnstableApiUsage")
@@ -9,10 +12,12 @@ class SubscribeRequest(override val URL: String?, override val httpMethod: Strin
     var topic: String? = null
     var uri: URI? = null
     var contentType: String? = null
+    var acceptType: String? = null;
 
     init {
         topic = URL?.trim()?.trim('/')
         contentType = headers.getOrDefault("Content-Type", "text/plain")
+        acceptType = headers.getOrDefault("Accept", "text/plain")
         uri = headers["Host"]?.let {
             URI.create(it)
         }
@@ -24,6 +29,14 @@ class SubscribeRequest(override val URL: String?, override val httpMethod: Strin
 
     fun bodyBytes(): ByteArray {
         return textToSend?.encodeToByteArray() ?: ByteArray(0)
+    }
+
+    fun getMessageBodyFiletHint(fileBaseName: String): CommonClientBodyFileHint {
+        return if (acceptType!!.contains("json")) {
+            JsonBodyFileHint.jsonBodyFileHint("${fileBaseName}.json")
+        } else {
+            TextBodyFileHint.textBodyFileHint("${fileBaseName}.txt")
+        }
     }
 
 }
