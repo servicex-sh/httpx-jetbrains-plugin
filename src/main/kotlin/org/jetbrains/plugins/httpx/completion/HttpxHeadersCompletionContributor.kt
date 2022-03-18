3f0cfc19007ed91376aa64133200ef9ec5f21bd7
linux_china <libing.chen@gmail.com>
2022-03-18 13:21:35 -0700

    HttpxHeadersCompletionContributor

---
+++ src/main/kotlin/org/jetbrains/plugins/httpx/completion/HttpxHeadersCompletionContributor.kt
@@ -0,0 +1,23 @@
+package org.jetbrains.plugins.httpx.completion
+
+import com.intellij.codeInsight.completion.*
+import com.intellij.codeInsight.lookup.LookupElementBuilder
+import com.intellij.httpClient.http.request.psi.HttpHeaderFieldName
+import com.intellij.patterns.PlatformPatterns.psiElement
+import com.intellij.util.ProcessingContext
+
+class HttpxHeadersCompletionContributor : CompletionContributor() {
+    init {
+        extend(CompletionType.BASIC, psiElement().withParent(HttpHeaderFieldName::class.java), HttpxHeaderFieldNamesProvider())
+    }
+
+    private class HttpxHeaderFieldNamesProvider : CompletionProvider<CompletionParameters>() {
+        override fun addCompletions(parameters: CompletionParameters, context: ProcessingContext, result: CompletionResultSet) {
+            result.addElement(LookupElementBuilder.create("Subject"))
+            result.addElement(LookupElementBuilder.create("Reply-To"))
+            result.addElement(LookupElementBuilder.create("X-JSON-Schema"))
+            result.addElement(LookupElementBuilder.create("X-JSON-Type"))
+            result.addElement(LookupElementBuilder.create("X-Java-Type"))
+        }
+    }
+}
