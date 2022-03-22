b5b693f74256b3901ff2f9398bdc58d23acc6513
linux_china <libing.chen@gmail.com>
2022-03-22 14:38:21 -0700

    Polishiing

--- src/main/kotlin/org/jetbrains/plugins/httpx/json/GraphqlRequestJsonSchemaProviderFactory.kt
+++ src/main/kotlin/org/jetbrains/plugins/httpx/json/GraphqlRequestJsonSchemaProviderFactory.kt
@@ -1,33 +1,8 @@
 package org.jetbrains.plugins.httpx.json
 
-import com.intellij.httpClient.http.request.psi.HttpMessageBody
-import com.intellij.httpClient.http.request.psi.HttpRequest
-import com.intellij.json.psi.JsonFile
-import com.intellij.lang.injection.InjectedLanguageManager
-import com.intellij.openapi.vfs.VfsUtil
-import com.intellij.openapi.vfs.VirtualFile
-import com.intellij.psi.PsiFile
-import com.intellij.psi.util.PsiTreeUtil
-import com.jetbrains.jsonSchema.extension.ContentAwareJsonSchemaFileProvider
-
 @Suppress("UnstableApiUsage")
-class GraphqlRequestJsonSchemaProviderFactory : ContentAwareJsonSchemaFileProvider {
-    override fun getSchemaFile(psiFile: PsiFile): VirtualFile? {
-        if (psiFile is JsonFile) {
-            val injectedLanguageManager = InjectedLanguageManager.getInstance(psiFile.project)
-            val psiLanguageInjectionHost = injectedLanguageManager.getInjectionHost(psiFile)
-            if (psiLanguageInjectionHost is HttpMessageBody) {
-                val httpMessageBody: HttpMessageBody = psiLanguageInjectionHost
-                val httpRequest = PsiTreeUtil.getParentOfType(httpMessageBody, HttpRequest::class.java)!!
-                val headerField = httpRequest.getHeaderField("Content-Type")
-                if (headerField != null) {
-                    val contentType = headerField.headerFieldValue?.text
-                    if (contentType != null && contentType.contains("application/graphql+json")) {
-                        return VfsUtil.findFileByURL(this.javaClass.getResource("/graphql-request-schema.json")!!)
-                    }
-                }
-            }
-        }
-        return null
-    }
+class GraphqlRequestJsonSchemaProviderFactory : ContentTypeAwareJsonSchemaFileProvider(
+    "application/graphql+json", "/graphql-request-schema.json"
+) {
+
 }
