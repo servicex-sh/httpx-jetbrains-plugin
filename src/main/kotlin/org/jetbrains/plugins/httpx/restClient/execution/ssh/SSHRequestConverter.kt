36f508074033b08ab9f2093b4862711dfe66887e
linux_china <libing.chen@gmail.com>
2022-03-23 14:20:49 -0700

    SSHRequestConverter

---
+++ src/main/kotlin/org/jetbrains/plugins/httpx/restClient/execution/ssh/SSHRequestConverter.kt
@@ -0,0 +1,38 @@
+package org.jetbrains.plugins.httpx.restClient.execution.ssh
+
+import com.intellij.httpClient.execution.common.RequestConverter
+import com.intellij.httpClient.http.request.HttpRequestVariableSubstitutor
+import com.intellij.httpClient.http.request.psi.HttpRequest
+import com.intellij.openapi.application.ApplicationManager
+import com.intellij.psi.SmartPsiElementPointer
+
+@Suppress("UnstableApiUsage")
+class SSHRequestConverter : RequestConverter<SSHRequest>() {
+
+    override val requestType: Class<SSHRequest> get() = SSHRequest::class.java
+
+    override fun psiToCommonRequest(requestPsiPointer: SmartPsiElementPointer<HttpRequest>, substitutor: HttpRequestVariableSubstitutor): SSHRequest {
+        var url = ""
+        var requestType = "SSH" // 6 chars
+        var requestBody: String? = null
+        lateinit var headers: Map<String, String>
+        ApplicationManager.getApplication().runReadAction {
+            val httpRequest = requestPsiPointer.element!!
+            url = "ssh://" + httpRequest.getHttpUrl(substitutor)!!
+            headers = httpRequest.headerFieldList.associate { it.name to it.getValue(substitutor) }
+            requestType = httpRequest.httpMethod
+            requestBody = httpRequest.requestBody?.text
+        }
+        return SSHRequest(url, requestType, requestBody, headers)
+    }
+
+    override fun toExternalFormInner(request: SSHRequest, fileName: String?): String {
+        val builder = StringBuilder()
+        builder.append("### SSH request").append("\n")
+        builder.append("SSH ${request.URL}").append("\n")
+        builder.append("\n");
+        builder.append(request.textToSend ?: "")
+        return builder.toString()
+    }
+
+}
