edc6a1b31da0095c3ca6757181965197d24113f8
linux_china <libing.chen@gmail.com>
2022-03-23 14:22:17 -0700

    SSHRequestExecutionSupport

---
+++ src/main/kotlin/org/jetbrains/plugins/httpx/restClient/execution/ssh/SSHRequestExecutionSupport.kt
@@ -0,0 +1,25 @@
+package org.jetbrains.plugins.httpx.restClient.execution.ssh
+
+import com.intellij.httpClient.execution.common.RequestContext
+import com.intellij.httpClient.execution.common.RequestConverter
+import com.intellij.httpClient.execution.common.RequestExecutionSupport
+import com.intellij.httpClient.execution.common.RequestHandler
+
+@Suppress("UnstableApiUsage")
+class SSHRequestExecutionSupport : RequestExecutionSupport<SSHRequest> {
+    override fun canProcess(requestContext: RequestContext): Boolean {
+        return requestContext.method == "SSH"
+    }
+
+    override fun getRequestConverter(): RequestConverter<SSHRequest> {
+        return SSHRequestConverter()
+    }
+
+    override fun getRequestHandler(): RequestHandler<SSHRequest> {
+        return SSHRequestHandler()
+    }
+
+    override fun supportedMethods(): Collection<String> {
+        return listOf("SSH")
+    }
+}
