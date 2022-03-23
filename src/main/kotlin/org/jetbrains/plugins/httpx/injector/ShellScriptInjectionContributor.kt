885418598eda2b9fb38d7f64f59580ecaa42b4a6
linux_china <libing.chen@gmail.com>
2022-03-23 14:23:36 -0700

    ShellScriptInjectionContributor

---
+++ src/main/kotlin/org/jetbrains/plugins/httpx/injector/ShellScriptInjectionContributor.kt
@@ -0,0 +1,34 @@
+package org.jetbrains.plugins.httpx.injector
+
+import com.intellij.httpClient.http.request.psi.HttpMessageBody
+import com.intellij.httpClient.http.request.psi.HttpRequest
+import com.intellij.lang.Language
+import com.intellij.lang.injection.general.Injection
+import com.intellij.lang.injection.general.LanguageInjectionContributor
+import com.intellij.lang.injection.general.SimpleInjection
+import com.intellij.psi.PsiElement
+import org.jetbrains.kotlin.psi.psiUtil.getParentOfType
+
+/**
+ * language injection for SSH method with shell script
+ */
+class ShellScriptInjectionContributor : LanguageInjectionContributor {
+    var shellLanguage: Language? = null
+
+    init {
+        shellLanguage = Language.findLanguageByID("Shell Script");
+        if (shellLanguage == null) {
+            shellLanguage = Language.findLanguageByID("BashPro Shell Script")
+        }
+    }
+
+    override fun getInjection(context: PsiElement): Injection? {
+        if (shellLanguage != null && context is HttpMessageBody) {
+            val httpRequest = context.getParentOfType<HttpRequest>(false)
+            if (httpRequest != null && httpRequest.httpMethod == "SSH") {
+                return SimpleInjection(shellLanguage!!, "", "", null);
+            }
+        }
+        return null
+    }
+}
