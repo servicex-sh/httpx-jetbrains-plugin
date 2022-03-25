bd191e8f42b215fb5b1ff2d8dea466b078d92a76
linux_china <libing.chen@gmail.com>
2022-03-24 22:09:45 -0700

    Bundle slf4j-api

--- build.gradle.kts
+++ build.gradle.kts
@@ -82,7 +82,6 @@ dependencies {
 
 configurations.implementation {
     exclude(group = "org.jetbrains", module = "annotations")
-    exclude(group = "org.slf4j", module = "slf4j-api")
     exclude(group = "io.netty", module = "netty-resolver-dns-native-macos")
     exclude(group = "io.netty", module = "netty-transport-native-epoll")
     exclude(group = "io.projectreactor.netty.incubator", module = "reactor-netty-incubator-quic")
