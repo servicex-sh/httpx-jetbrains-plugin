56ee9f7993d52c7a66114e77350f756898f4d86e
linux_china <libing.chen@gmail.com>
2022-03-29 13:17:00 -0700

    Bug fix mqtt5 schema

--- src/main/kotlin/org/jetbrains/plugins/httpx/restClient/execution/common/PubSubUtils.kt
+++ src/main/kotlin/org/jetbrains/plugins/httpx/restClient/execution/common/PubSubUtils.kt
@@ -2,13 +2,13 @@ package org.jetbrains.plugins.httpx.restClient.execution.common
 
 import java.net.URI
 
-data class UriAndSubject(val uri: String, val subject: String)
-
 fun getMqttUri(mqttURI: URI): String {
     val schema = mqttURI.scheme
     var brokerUrl = mqttURI.toString()
     brokerUrl = if (schema.contains("+")) {
         brokerUrl.substring(brokerUrl.indexOf("+") + 1)
+    } else if (schema == "mqtt5") {
+        brokerUrl.replace("mqtt5://", "tcp://")
     } else {
         brokerUrl.replace("mqtt://", "tcp://")
     }
