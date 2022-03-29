4a8fd28f7e1138377c7cb7440b575bc129dab1f0
linux_china <libing.chen@gmail.com>
2022-03-29 16:44:25 -0700

    ProductTest

---
+++ src/test/kotlin/org/jetbrains/plugins/httpx/restClient/execution/aliyun/ProductTest.kt
@@ -0,0 +1,24 @@
+package org.jetbrains.plugins.httpx.restClient.execution.aliyun
+
+import com.fasterxml.jackson.module.kotlin.readValue
+import org.jetbrains.plugins.httpx.json.JsonUtils.objectMapper
+import org.junit.Test
+import java.net.URL
+
+class ProductTest {
+    @Test
+    fun testParse() {
+        val products = Products.instance()
+        println(products.products.size)
+        val arms = products.findProduct("arms")!!
+        val action = arms.findAction("ConfigApp")!!
+        println(action.convertToJsonSchema("arms"))
+    }
+
+    @Test
+    fun testActionParse() {
+        val url = "https://raw.githubusercontent.com/aliyun/aliyun-openapi-meta/master/metadatas/arms/ConfigApp.json";
+        val action = objectMapper.readValue<Action>(URL(url))
+        println(action.parameters.size)
+    }
+}
