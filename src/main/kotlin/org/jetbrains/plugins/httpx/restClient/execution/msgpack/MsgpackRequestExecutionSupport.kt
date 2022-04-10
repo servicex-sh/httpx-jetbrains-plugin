9d67f11f079c575d4246beb1a4d3e2a0d8bf71f2
linux_china <libing.chen@gmail.com>
2022-04-09 18:50:19 -0700

    MsgpackRequestExecutionSupport

---
+++ src/main/kotlin/org/jetbrains/plugins/httpx/restClient/execution/msgpack/MsgpackRequestExecutionSupport.kt
@@ -0,0 +1,25 @@
+package org.jetbrains.plugins.httpx.restClient.execution.msgpack
+
+import com.intellij.httpClient.execution.common.RequestContext
+import com.intellij.httpClient.execution.common.RequestConverter
+import com.intellij.httpClient.execution.common.RequestExecutionSupport
+import com.intellij.httpClient.execution.common.RequestHandler
+
+@Suppress("UnstableApiUsage")
+class MsgpackRequestExecutionSupport : RequestExecutionSupport<MsgpackRequest> {
+    override fun canProcess(requestContext: RequestContext): Boolean {
+        return requestContext.method == "MSGPACK"
+    }
+
+    override fun getRequestConverter(): RequestConverter<MsgpackRequest> {
+        return MsgpackRequestConverter()
+    }
+
+    override fun getRequestHandler(): RequestHandler<MsgpackRequest> {
+        return MsgpackRequestHandler()
+    }
+
+    override fun supportedMethods(): Collection<String> {
+        return listOf("MSGPACK")
+    }
+}
