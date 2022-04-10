16d7186e20169c70249dbb98469d667be894e4c9
linux_china <libing.chen@gmail.com>
2022-04-09 18:50:10 -0700

    MsgpackRequestConverter

---
+++ src/main/kotlin/org/jetbrains/plugins/httpx/restClient/execution/msgpack/MsgpackRequestConverter.kt
@@ -0,0 +1,41 @@
+package org.jetbrains.plugins.httpx.restClient.execution.msgpack
+
+import com.intellij.httpClient.execution.common.RequestConverter
+import com.intellij.httpClient.http.request.HttpRequestVariableSubstitutor
+import com.intellij.httpClient.http.request.psi.HttpRequest
+import com.intellij.openapi.application.ApplicationManager
+import com.intellij.psi.SmartPsiElementPointer
+import org.jetbrains.plugins.httpx.restClient.execution.tarpc.TarpcRequest
+
+
+@Suppress("UnstableApiUsage")
+class MsgpackRequestConverter : RequestConverter<MsgpackRequest>() {
+
+    override val requestType: Class<MsgpackRequest> get() = MsgpackRequest::class.java
+
+    override fun psiToCommonRequest(requestPsiPointer: SmartPsiElementPointer<HttpRequest>, substitutor: HttpRequestVariableSubstitutor): MsgpackRequest {
+        var url = ""
+        var requestType = "MSGPACK"
+        var requestBody: String? = null
+        lateinit var headers: Map<String, String>
+        ApplicationManager.getApplication().runReadAction {
+            val httpRequest = requestPsiPointer.element!!
+            url = "msgpack://" + httpRequest.getHttpUrl(substitutor)!!
+            headers = httpRequest.headerFieldList.associate { it.name to it.getValue(substitutor) }
+            requestType = httpRequest.httpMethod
+            requestBody = httpRequest.requestBody?.text
+        }
+        return MsgpackRequest(url, requestType, requestBody, headers)
+    }
+
+    override fun toExternalFormInner(request: MsgpackRequest, fileName: String?): String {
+        val builder = StringBuilder()
+        builder.append("### msgpack request").append("\n")
+        builder.append("MSGPACK ${request.URL}").append("\n")
+        builder.append("Content-Type: application/json").append("\n")
+        builder.append("\n")
+        builder.append(request.textToSend ?: "")
+        return builder.toString()
+    }
+
+}
