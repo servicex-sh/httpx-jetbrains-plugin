130558c34d24db4752d6cd94097acb12d96b2d50
linux_china <libing.chen@gmail.com>
2022-04-09 18:49:31 -0700

    MsgpackResponse

---
+++ src/main/kotlin/org/jetbrains/plugins/httpx/restClient/execution/msgpack/MsgpackResponse.kt
@@ -0,0 +1,38 @@
+package org.jetbrains.plugins.httpx.restClient.execution.msgpack
+
+import com.intellij.httpClient.execution.common.CommonClientResponse
+import com.intellij.httpClient.execution.common.CommonClientResponseBody
+import com.intellij.json.JsonFileType
+import com.intellij.openapi.fileTypes.FileType
+
+@Suppress("UnstableApiUsage")
+class MsgpackResponse(
+    private val responseBody: CommonClientResponseBody = CommonClientResponseBody.Empty(),
+    private val status: String = "OK",
+    private val error: String? = null,
+    override var executionTime: Long? = 0
+) : CommonClientResponse {
+    override val body: CommonClientResponseBody
+        get() = responseBody
+
+    override fun suggestFileTypeForPresentation(): FileType {
+        return JsonFileType.INSTANCE
+    }
+
+    override val statusPresentation: String
+        get() = status
+
+    override val presentationHeader: String
+        get() {
+            return if (status == "OK") {
+                "msgpack 200 OK\n"
+            } else {
+                "msgpack ERROR\n${(error ?: "")}\n"
+            }
+        }
+
+    override val presentationFooter: String
+        get() {
+            return "Execution time: $executionTime ms"
+        }
+}
