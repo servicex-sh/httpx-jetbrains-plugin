eead23ac92263d28531da9313a6d946eee5bcabf
linux_china <libing.chen@gmail.com>
2022-04-11 14:12:20 -0700

    JsonRpcRequestExecutionSupport

---
+++ src/main/kotlin/org/jetbrains/plugins/httpx/restClient/execution/jsonrpc/JsonRpcRequestExecutionSupport.kt
@@ -0,0 +1,25 @@
+package org.jetbrains.plugins.httpx.restClient.execution.jsonrpc
+
+import com.intellij.httpClient.execution.common.RequestContext
+import com.intellij.httpClient.execution.common.RequestConverter
+import com.intellij.httpClient.execution.common.RequestExecutionSupport
+import com.intellij.httpClient.execution.common.RequestHandler
+
+@Suppress("UnstableApiUsage")
+class JsonRpcRequestExecutionSupport : RequestExecutionSupport<JsonRpcRequest> {
+    override fun canProcess(requestContext: RequestContext): Boolean {
+        return requestContext.method == "JSONRPC"
+    }
+
+    override fun getRequestConverter(): RequestConverter<JsonRpcRequest> {
+        return JsonRpcRequestConverter()
+    }
+
+    override fun getRequestHandler(): RequestHandler<JsonRpcRequest> {
+        return JsonRpcRequestHandler()
+    }
+
+    override fun supportedMethods(): Collection<String> {
+        return listOf("JSONRPC")
+    }
+}
