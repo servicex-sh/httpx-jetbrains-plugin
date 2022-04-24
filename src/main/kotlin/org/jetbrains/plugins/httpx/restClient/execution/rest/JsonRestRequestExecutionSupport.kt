d4fc38802cab87ae8c570a12bb295ea165fa2ad3
linux_china <libing.chen@gmail.com>
2022-04-24 12:26:22 -0700

    JsonRestRequestExecutionSupport

---
+++ src/main/kotlin/org/jetbrains/plugins/httpx/restClient/execution/rest/JsonRestRequestExecutionSupport.kt
@@ -0,0 +1,27 @@
+package org.jetbrains.plugins.httpx.restClient.execution.rest
+
+import com.intellij.httpClient.execution.RestClientRequest
+import com.intellij.httpClient.execution.common.RequestContext
+import com.intellij.httpClient.execution.common.RequestConverter
+import com.intellij.httpClient.execution.common.RequestExecutionSupport
+import com.intellij.httpClient.execution.common.RequestHandler
+import com.intellij.httpClient.execution.impl.HttpRequestHandler
+
+@Suppress("UnstableApiUsage")
+class JsonRestRequestExecutionSupport : RequestExecutionSupport<RestClientRequest> {
+    override fun canProcess(requestContext: RequestContext): Boolean {
+        return requestContext.method == "REST"
+    }
+
+    override fun getRequestConverter(): RequestConverter<RestClientRequest> {
+        return JsonRestRequestConverter()
+    }
+
+    override fun getRequestHandler(): RequestHandler<RestClientRequest> {
+        return HttpRequestHandler()
+    }
+
+    override fun supportedMethods(): Collection<String> {
+        return listOf("REST")
+    }
+}
