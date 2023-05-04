3db561e173337fdde6bda8ec7ac98645383efa1f
Libing Chen <libing.chen@gmail.com>
2023-05-04 08:41:56 +0800

    app: ChatgptInjectionContributor

---
+++ src/main/kotlin/org/jetbrains/plugins/httpx/injector/ChatgptInjectionContributor.kt
@@ -0,0 +1,27 @@
+package org.jetbrains.plugins.httpx.injector
+
+import com.intellij.httpClient.http.request.psi.HttpMessageBody
+import com.intellij.httpClient.http.request.psi.HttpRequest
+import com.intellij.lang.Language
+import com.intellij.lang.injection.general.Injection
+import com.intellij.lang.injection.general.LanguageInjectionContributor
+import com.intellij.lang.injection.general.SimpleInjection
+import com.intellij.psi.PsiElement
+import org.jetbrains.kotlin.psi.psiUtil.getParentOfType
+
+/**
+ * language injection for SSH method with shell script
+ */
+class ChatgptInjectionContributor : LanguageInjectionContributor {
+    private val mdLanguage: Language = Language.findLanguageByID("Markdown")!!
+
+    override fun getInjection(context: PsiElement): Injection? {
+        if (context is HttpMessageBody) {
+            val httpRequest = context.getParentOfType<HttpRequest>(false)
+            if (httpRequest != null && httpRequest.httpMethod == "CHATGPT") {
+                return SimpleInjection(mdLanguage, "", "", null);
+            }
+        }
+        return null
+    }
+}
