d89faf5024be3e1ef585d84580a1a4af84a60db8
Libing Chen <libing.chen@gmail.com>
2023-06-16 21:32:33 +0800

    app: OpenAIRequestJsonSchemaProviderFactory

---
+++ src/main/kotlin/org/jetbrains/plugins/httpx/json/OpenAIRequestJsonSchemaProviderFactory.kt
@@ -0,0 +1,34 @@
+package org.jetbrains.plugins.httpx.json
+
+import com.intellij.httpClient.http.request.psi.HttpMessageBody
+import com.intellij.httpClient.http.request.psi.HttpRequest
+import com.intellij.json.psi.JsonFile
+import com.intellij.lang.injection.InjectedLanguageManager
+import com.intellij.openapi.vfs.VfsUtil
+import com.intellij.openapi.vfs.VirtualFile
+import com.intellij.psi.PsiFile
+import com.intellij.psi.util.PsiTreeUtil
+import com.jetbrains.jsonSchema.extension.ContentAwareJsonSchemaFileProvider
+
+@Suppress("UnstableApiUsage")
+class OpenAIRequestJsonSchemaProviderFactory : ContentAwareJsonSchemaFileProvider {
+    override fun getSchemaFile(psiFile: PsiFile): VirtualFile? {
+        if (psiFile is JsonFile) {
+            val injectedLanguageManager = InjectedLanguageManager.getInstance(psiFile.project)
+            val psiLanguageInjectionHost = injectedLanguageManager.getInjectionHost(psiFile)
+            if (psiLanguageInjectionHost is HttpMessageBody) {
+                val httpMessageBody: HttpMessageBody = psiLanguageInjectionHost
+                val httpRequest = PsiTreeUtil.getParentOfType(httpMessageBody, HttpRequest::class.java)!!
+                if (httpRequest.httpMethod == "POST") {
+                    val targetText = httpRequest.requestTarget?.text ?: ""
+                    if (targetText.startsWith("https://api.openai.com/v1/chat/completions")) {
+                        return VfsUtil.findFileByURL(this.javaClass.getResource("/chatgpt-request-schema.json")!!)
+                    } else if (targetText.startsWith("https://api.openai.com/v1/completions")) {
+                        return VfsUtil.findFileByURL(this.javaClass.getResource("/openai-completion-request-schema.json")!!)
+                    }
+                }
+            }
+        }
+        return null
+    }
+}
